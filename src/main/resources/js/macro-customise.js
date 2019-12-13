61e23737e4a96f6692feeef2424e200dd91d059c
PengXiao <eagle.xiao@gmail.com>
2019-12-13 17:21:22 +1100

    Editor mounted.

--- src/main/resources/js/macro-customise.js
+++ src/main/resources/js/macro-customise.js
@@ -1,36 +1,39 @@
 AJS.bind('init.rte', function () {
-    let macroName = 'sequence';
-    // The dialog is defined in zenuml-editor-dialog.vm
-    let dialog = AJS.dialog2("#zenuml-editor-dialog");
-    AJS.$("#zenuml-editor-dialog-submit-button").click(function (e) {
-      var newParams = {};
-      // dsl = zenUmlStore;
-      newParams["dsl"] = 'A.method(from, macro)';
-      var macro = {
-        name: macroName,
-        params: newParams,
-        defaultParameterValue: "",
-        body : ""
-      };
-      var selection = AJS.Rte.getEditor().selection.getNode();
-      tinymce.plugins.Autoconvert.convertMacroToDom(macro, function(data, textStatus, jqXHR ) {
-        AJS.$(selection).replaceWith(data + " <p>Replaced! something<br/>something else</p>");
-        dialog.hide();
-      }, function(jqXHR, textStatus, errorThrown ) {
-        AJS.log("error converting macro to DOM");
-      });
+  // mount the editor
+  VueModel.$mount('#app');
+  let macroName = 'sequence';
+  // The dialog is defined in zenuml-editor-dialog.vm
+  let dialog = AJS.dialog2("#zenuml-editor-dialog");
+  AJS.$("#zenuml-editor-dialog-submit-button").click(function (e) {
+    var newParams = {};
+    // dsl = zenUmlStore;
+    newParams["dsl"] = 'A.method(from, macro)';
+    var macro = {
+      name: macroName,
+      params: newParams,
+      defaultParameterValue: "",
+      body: ""
+    };
+    var selection = AJS.Rte.getEditor().selection.getNode();
+    tinymce.plugins.Autoconvert.convertMacroToDom(macro, function (data, textStatus, jqXHR) {
+      AJS.$(selection).replaceWith(data + " <p>Replaced! something<br/>something else</p>");
+      dialog.hide();
+    }, function (jqXHR, textStatus, errorThrown) {
+      AJS.log("error converting macro to DOM");
     });
+  });
 
-    // override opener: get old DSL from data-macro-parameters and set it to zenUmlStore
-    AJS.MacroBrowser.setMacroJsOverride('sequence', {opener: function(macro) {
-        console.log('macro', macro);
-        // dialog.store.macor = macro;
-        // open custom dialog
-        dialog.show();
-    }});
+  // override opener: get old DSL from data-macro-parameters and set it to zenUmlStore
+  AJS.MacroBrowser.setMacroJsOverride('sequence', {
+    opener: function (macro) {
+      console.log('macro', macro);
+      // dialog.store.macor = macro;
+      // open custom dialog
+      dialog.show();
+    }
+  });
 
 });
-
 
 
 // AJS.bind("init.rte", function() {
