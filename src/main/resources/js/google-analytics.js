73d6c2c2b4a5e6fd13596e6b9c5b5c8e451effd1
zheng gong <zhenggong@zhengs-MacBook-Pro.local>
2019-12-24 11:21:42 +1100

    feat(analytics): import gtag.js into project

---
+++ src/main/resources/js/google-analytics.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+// GA_MEASUREMENT_ID need to be replaced
+gtag('config', 'GA_MEASUREMENT_ID');
