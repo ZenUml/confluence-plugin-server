45bb1ace5f5d961499d1a721e9d15277575d4038
zheng gong <zhenggong@zhengs-MacBook-Pro.local>
2019-12-27 11:40:58 +1100

    feat(analytics): bind create event to button

--- src/main/resources/js/google-analytics.js
+++ src/main/resources/js/google-analytics.js
@@ -1,5 +1,10 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-// GA_MEASUREMENT_ID need to be replaced
-gtag('config', 'GA_MEASUREMENT_ID');
+$(function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  // GA_MEASUREMENT_ID need to be replaced
+  gtag('config', '');
+  AJS.$("#zenuml-editor-dialog-submit-button").click(function(){
+    gtag('event', 'zenuml_create_success');
+  });
+});
