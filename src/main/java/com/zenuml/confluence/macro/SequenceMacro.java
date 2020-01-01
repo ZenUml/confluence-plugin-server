a4ec0f7919384b9aef1d29bf9a8877ea7eb10bd0
PengXiao <eagle.xiao@gmail.com>
2020-01-02 08:31:15 +1100

    Fixed licensing issue (not able to enable the plugin).

--- src/main/java/com/zenuml/confluence/macro/SequenceMacro.java
+++ src/main/java/com/zenuml/confluence/macro/SequenceMacro.java
@@ -4,6 +4,7 @@ import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.*;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.upm.api.license.PluginLicenseManager;
 import com.atlassian.webresource.api.assembler.PageBuilderService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,16 +14,22 @@ import java.util.Map;
 public class SequenceMacro implements Macro {
 
     private PageBuilderService pageBuilderService;
+    private final PluginLicenseManager pluginLicenseManager;
 
     @Autowired
-    public SequenceMacro(@ComponentImport PageBuilderService pageBuilderService) {
+    public SequenceMacro(@ComponentImport PageBuilderService pageBuilderService, @ComponentImport PluginLicenseManager pluginLicenseManager) {
         this.pageBuilderService = pageBuilderService;
+        this.pluginLicenseManager = pluginLicenseManager;
     }
 
 
     public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
         pageBuilderService.assembler().resources().requireWebResource("com.zenuml.confluence.sequence:active-sequence-resources");
-        return String.join("", "<sequence-diagram>", s, "</sequence-diagram>");
+        String license = "Missing";
+        if (pluginLicenseManager.getLicense().isDefined()) {
+            license = "Presented";
+        }
+        return String.join("", "<sequence-diagram>", s, "</sequence-diagram>", license);
     }
     public BodyType getBodyType() { return BodyType.PLAIN_TEXT; }
 
