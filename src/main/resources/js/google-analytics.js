29fb8d43d1bda2a07b0ce5114255422ba83ffdac
zheng gong <zhenggong@192-168-1-103.tpgi.com.au>
2020-01-05 16:55:55 +1100

    fix(analytics): update gtag.js by adding script tag dynamically

--- src/main/resources/js/google-analytics.js
+++ src/main/resources/js/google-analytics.js
@@ -1,9 +1,16 @@
 $(function() {
+  // GA_MEASUREMENT_ID is the tracking ID
+  var GA_MEASUREMENT_ID = "";
+  var s = document.createElement('script');
+  s.type = "text/javascript";
+  s.src = 'https://www.googletagmanager.com/gtag/js?id='+GA_MEASUREMENT_ID;
+  $('head').append(s);
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  // GA_MEASUREMENT_ID need to be replaced
-  gtag('config', '');
+
+  gtag('config', GA_MEASUREMENT_ID);
+  
   $("#zenuml-editor-dialog-submit-button").click(function(){
     gtag('event', 'zenuml_create_success');
   });
