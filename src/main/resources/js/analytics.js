60ef0e694f79b5e70fa93d0d4f0d67591b827e65
zheng gong <zhenggong@zhengs-MacBook-Pro.local>
2020-02-05 11:08:23 +1100

    fix(analytics): warp functions

--- src/main/resources/js/analytics.js
+++ src/main/resources/js/analytics.js
@@ -1,16 +1,16 @@
 $(function() {  
-  //Google TagManager
-  var GTM_ID = 'GTM-PVFGNPM';
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer',GTM_ID);
-
-  var tgmIframe = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id='+GTM_ID+'"'+
-  ' height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
-  $(document.body).prepend(tgmIframe)
-  //GetBrowserType
+  function TagManagerSetup () {
+    var GTM_ID = 'GTM-PVFGNPM';
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer',GTM_ID);
+  
+    var tgmIframe = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id='+GTM_ID+'"'+
+    ' height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
+    $(document.body).prepend(tgmIframe)
+  }
   navigator.sayswho= (function(){
     var ua= navigator.userAgent, tem, 
     M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
@@ -26,29 +26,32 @@ $(function() {
     if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
     return M.join(' ');
   })();
-  //DataLayer of GTM
-  var baseUrl = AJS.Confluence.getBaseUrl();
-  var browserVersion = navigator.sayswho;
-  var confluenceVersion = AJS.Meta.get("version-number");
-  var serverName = AJS.Meta.get("server-name");
-  var userFullName = AJS.Meta.get("current-user-fullname");
-  AJS.$.ajax({
-    url: baseUrl+ '/rest/plugins/1.0/com.zenuml.confluence-addon-key/summary',
-    type: 'Get',
-    beforeSend: function (request) {
-      request.setRequestHeader("X-Atlassian-Token", "nocheck");
-    },
-    processData: false,
-    contentType: false,
-  }).then(function(res) {
-    dataLayer.push({
-      UserFullName: userFullName,
-      ServerName: serverName,
-      BrowserVersion: browserVersion,
-      ConfluenceVersion: confluenceVersion,
-      ZenUMLVersion: res.version,
-      UsesLicensing: res.usesLicensing===true ? 'N':'Y',
-      event: "UserStatus"
-    })
-  });
+  function dataLayerSetup() {
+    var baseUrl = AJS.Confluence.getBaseUrl();
+    var browserVersion = navigator.sayswho;
+    var confluenceVersion = AJS.Meta.get("version-number");
+    var serverName = AJS.Meta.get("server-name");
+    var userFullName = AJS.Meta.get("current-user-fullname");
+    AJS.$.ajax({
+      url: baseUrl+ '/rest/plugins/1.0/com.zenuml.confluence-addon-key/summary',
+      type: 'Get',
+      beforeSend: function (request) {
+        request.setRequestHeader("X-Atlassian-Token", "nocheck");
+      },
+      processData: false,
+      contentType: false,
+    }).then(function(res) {
+      dataLayer.push({
+        UserFullName: userFullName,
+        ServerName: serverName,
+        BrowserVersion: browserVersion,
+        ConfluenceVersion: confluenceVersion,
+        ZenUMLVersion: res.version,
+        UsesLicensing: res.usesLicensing===true ? 'N':'Y',
+        event: "UserStatus"
+      })
+    });
+  }
+  TagManagerSetup();
+  dataLayerSetup();
 });
