a5abec150b42ca3de8cc8dffcfc58bdcceec1be1
PengXiao <eagle.xiao@gmail.com>
2021-02-13 12:52:08 +1100

    Removed debugger and unused functions.

--- src/main/resources/js/macro-customise.js
+++ src/main/resources/js/macro-customise.js
@@ -42,48 +42,16 @@ AJS.bind('init.rte', function () {
 });
 
 //================VIEWER========================
+// We have to do attachment upload in Viewer but not Editor because users can change the code without going into the Editor.
 AJS.$(document).ready(function () {
-  debugger
   var pageId = AJS.Meta.get("page-id");
   var baseUrl = AJS.Confluence.getBaseUrl();
-  // Get all shadow elements from page
-  // return a dom node list
-  function getAllShadowElements() {
-    var elements = [];
-    AJS.$('diagram-as-code').each(function () {
-      if (this.shadowRoot.querySelector(".diagram-as-code.noEditor>.split>.sequence-diagram")) {
-        elements.push(this.shadowRoot.querySelector(".sequence-diagram"))
-      }
-    });
-    return elements;
-  }
-  // Get all Macro nodes and its dsl
-  // return dsl text and convertToBlob promise
-  function getConvertPromiseAndDsl() {
-    var doms = getAllShadowElements();
-    var objects = [];
-    if (doms.length > 0) {
-      var dslList = [];
-      doms.forEach(function (dom) {
-        var dslText = dom.__vue__.$store.state.code;
-        // remove duplicate when upload mutiple same uml at the same time
-        if (dslList.indexOf(dslText) < 0) {
-          objects.push({
-            dsl: dslText,
-            domtoimagePromise: domtoimage.toBlob(dom, { bgcolor: 'white' })
-          });
-          dslList.push(dslText);
-        }
-      })
-    }
-    return objects;
-  }
   // Get all attachments based on page id
   // return a ajax promise
   function getAttachments() {
     return AJS.$.ajax({
       url: baseUrl + '/rest/api/content/' + pageId + '/child/attachment?expand=version',
-      type: 'GET'
+        type: 'GET'
     });
   }
   // Create and Upload a new attchment
@@ -109,31 +77,18 @@ AJS.$(document).ready(function () {
   }
 
   setTimeout(function () {
-    var dslPromiseList = getConvertPromiseAndDsl();
-    var promises = [];
-    var dsls = [];
-    if (dslPromiseList.length > 0) {
-      dslPromiseList.forEach(function (item) {
-        promises.push(item.domtoimagePromise);
-        dsls.push(item.dsl);
-      });
-      Promise.all(promises).then(function (blobArray) {
-        getAttachments().then(function (res) {
-          var existingAttachments = res.results;
-          blobArray.forEach(function (blob, index) {
-            var dslhash = md5(dsls[index]);
-            var found = existingAttachments.filter(function (attachment) {
-              return attachment.title === "zenuml-" + dslhash;
-            });
-            if (found.length === 0) {
-              uploadAttachment(blob, dsls[index]).then(function (res) {
-                console.log("New Attachment is uploaded successfully");
-              });
-            }
-          });
-        });
-      });
-    }
-  }, 2000);
+    AJS.$('diagram-as-code.conf-macro.output-block').each(async function (index, el) {
+      const existingAttachments = await getAttachments();
+      const dsl = el.shadowRoot.querySelector('.sequence-diagram').__vue__.$store.state.code;
+      el.shadowRoot.querySelector('.sequence-diagram').__vue__.toBlob().then(function (blob) {
+        if (!existingAttachments.results.some(function (att) {
+          return att.title === 'zenuml-' +  md5(dsl)
+        })) {
+          uploadAttachment(blob, dsl);
+          console.log('Attachment updated.')
+        }
+      })
+    });
+    }, 2000);
 
 })
